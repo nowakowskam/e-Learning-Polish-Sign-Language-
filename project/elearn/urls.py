215c65582d2665a96ec9bbddf1ae71be6bdd6c5b
nowakowskam <monika-nowakowska@outlook.com>
2022-01-26 00:35:56 +0100

    solve test implemntation

--- project/elearn/urls.py
+++ project/elearn/urls.py
@@ -12,6 +12,7 @@ path('update_lesson/<int:pk>/',views.UpdateLessonView.as_view(), name='update_le
 path('test_list', views.TestListView.as_view(), name='test_list'),
 path('update_test/<int:pk>/', views.UpdateTestView.as_view(), name='update_test'),
 path('show_test/<int:pk>/', views.ShowTestView.as_view(), name='show_test'),
+path('solve_test/<int:pk>/', views.SolveTestView.as_view(), name='solve_test'),
 path('test_list/<int:pk>/test_confirm_delete', views.DeleteTestView.as_view(), name='delete_test'),
 path('list_lesson/<int:pk>/lesson_confirm_delete', views.LessonDeleteView.as_view(), name='delete_lesson')
 ]
