4cfda0624519f545876ca8fce56083c412ddcce7
nowakowskam <monika-nowakowska@outlook.com>
2022-01-28 14:05:11 +0100

    deleted learner, takentest, techaer model 2/2

--- project/elearn/urls.py
+++ project/elearn/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns=[
 path('create_lesson/', views.CreateLessonView.as_view(), name='create_lesson'),
-# path('create_lesson/<pk>/', views.CreateLessonView.as_view(), name='create_lesson'),
 path('create_test/', views.CreateTestView.as_view(), name='create_test'),
 path('list_lesson/', views.ListLessonView.as_view(), name='list_lesson'),
 path('show_lesson/<int:pk>/', views.ShowLessonView.as_view(), name='show_lesson'),
--- project/elearn/views.py
+++ project/elearn/views.py
@@ -1,18 +1,13 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
-from django.views.generic.edit import FormView
 from django.views.generic.edit import UpdateView
 from django.views.generic.list import ListView
 from django.views.generic import DetailView
 from django.views.generic import DeleteView
-from django.views.generic.detail import BaseDetailView
-from django_filters import FilterSet
 from django import http
 from django.contrib import messages
 from django.urls import reverse_lazy
 from django.urls import reverse
-from django.contrib.messages.views import SuccessMessageMixin
-from django.http import HttpResponseRedirect
 from .forms import LessonForm, TestCreateForm, CommentForm
 from accounts.models import User
 from accounts.models import Profile
@@ -21,7 +16,7 @@ from django.shortcuts import redirect
 
 
 from django.shortcuts import get_object_or_404
-from .models import Lesson, Test, Learner, Takentest, Teacher
+from .models import Lesson, Test
 
 class CreateLessonView(CreateView):
     form_class = LessonForm or None
@@ -141,10 +136,6 @@ class ShowLessonView(DetailView):
 
 
 
-    # def get_success_url(self, item):  # noqa: D102
-    #     pk = self.item.pk
-    #     return reverse_lazy('show_lesson', kwargs={'pk': pk})
-
 
 
 
@@ -153,25 +144,13 @@ class UpdateLessonView(UpdateView):
     form_class = LessonForm
     template_name = 'elearn/update_lesson.html'
 
-    # def get_context_data(self, **kwargs):
-    #     context = super(UpdateLessonView, self).get_context_data(**kwargs)
-    #     form = Lesson.objects.filter(pk=self.object.pk)
-    #     context['form'] = form
 
     def form_valid(self, form):
         form.save()
         messages.success(self.request, 'Zmiany zostały zapisane.')
         return super().form_valid(form)
 
-    # def get_context_data(self, **kwargs):  # noqa: D102
-    #     context = super(UpdateLessonView, self).get_context_data(**kwargs)
-    #     if 'form' not in context:
-    #         context['form'] = self.form_class(instance=self.object)
-    # def get_success_url(self,pk):  # noqa: D102
-    #     pk = self.object.id
-    #     return reverse_lazy('update_lesson', kwargs={'pk': self.object.pk})
 
-# pokazliste lekcji
 class ListLessonView(ListView):
     paginate_by = 5
     model = Lesson
@@ -247,9 +226,6 @@ class TestListView(ListView):
 
 
 
-
-
-# zmien test
 class UpdateTestView(UpdateView):
     model = Test
     form_class = TestCreateForm
